d1a118b81d119fde5a497368c274efd019323e75
Noman Haider <152289775+nomanhaider99@users.noreply.github.com>
2025-01-27 08:06:12 +0500

    change done

--- app/products/[productName]/page.tsx
+++ app/products/[productName]/page.tsx
@@ -4,7 +4,7 @@ import React from 'react';
 
 const ProductPage = async ({ params }: { params: { productName: string } }) => {
     console.log(params.productName)
-    const res = await fetch(`http://localhost:3000/api/products/${params.productName}`);
+    const res = await fetch(`/api/products/${params.productName}`);
 
     if (!res.ok) {
         const errorData = await res.json();
