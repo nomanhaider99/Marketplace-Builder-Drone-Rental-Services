75942a17957045879cb6142027cd03178c86b885
Noman Haider <152289775+nomanhaider99@users.noreply.github.com>
2025-01-28 22:48:20 +0500

    day 7 done

--- app/api/label/route.ts
+++ app/api/label/route.ts
@@ -10,7 +10,6 @@ export async function POST(request: Request) {
             state,
             country,
             phoneNumber,
-            // packages TODO: to add packages as well
         } = await request.json();
 
         if (!fullName || !address || !postalCode || !locality || !state || !country || !phoneNumber) {
